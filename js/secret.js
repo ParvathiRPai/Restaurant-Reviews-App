0cb0d1d3a757b44ae433d1a5778e0132ff74f62f
Parvathi Pai <parvathirpai@outlook.com>
2019-06-29 16:57:00 -0700

    stage-1

---
+++ js/secret.js
@@ -0,0 +1,6 @@
+class SECRET {
+    static get mapbox_key() {
+      // return your mapbox API key
+      return "pk.eyJ1IjoicnAxMiIsImEiOiJjanhnaTU2Zjcwd3R2M29wNnJha2x6c2c2In0.wbcwQIgkLjtlkmcAKxmRaA";
+    }
+  }
